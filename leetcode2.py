5b68bf231924d07b7ffc07ded5e49243ead5b088
Ariel <143091143+arielwongch@users.noreply.github.com>
2025-10-31 00:52:23 +0800

    leet

--- leetcode2.py
+++ leetcode2.py
@@ -27,4 +27,45 @@ The number of nodes in each linked list is in the range [1, 100].
 0 <= Node.val <= 9
 It is guaranteed that the list represents a number that does not have leading zeros.
 """
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+
+        head = ListNode()
+        current_ptr = head
+
+        l1_ptr = l1
+        l2_ptr = l2
+        remain = 0
+        while (l1_ptr is not None) or (l2_ptr is not None) or remain != 0:
+            
+            if l1_ptr is not None:
+                x = l1_ptr.val
+            else:
+                x = 0
+
+            if l2_ptr is not None:
+                y = l2_ptr.val
+            else:
+                y = 0
+            
+            sum = x + y +remain
+            remain = 0
+            if(sum>=10):
+                sum = sum - 10
+                remain = 1
+            current_ptr.next = ListNode(sum)
+
+            if l1_ptr is not None:
+                l1_ptr = l1_ptr.next
+            if l2_ptr is not None:
+                l2_ptr = l2_ptr.next
+
+            current_ptr = current_ptr.next
+
+        return head.next
