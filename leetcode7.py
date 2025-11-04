4680b0af105cde7d9a4de1bf351294d5fcae6877
Ariel <143091143+arielwongch@users.noreply.github.com>
2025-11-04 23:45:16 +0800

    Update leetcode7.py

--- leetcode7.py
+++ leetcode7.py
@@ -24,3 +24,39 @@ Constraints:
 -2^31 <= x <= 2^31 - 1
 """
 
+class Solution:
+    def reverse(self, x: int) -> int:
+
+        if x == 0:
+            return 0
+            
+        if x<0:
+            negative = 1
+            number = -1*x
+        else:
+            negative = 0
+            number = x
+        integer = ""
+        while number>=10:
+            digit = number%10
+            integer += str(digit)
+            number = number//10
+        integer += str(number)
+
+        while integer[0]==0:
+            integer = integer[1:]
+
+        n = len(integer)
+        output = 0
+
+        for i in range(n):
+            letter = int(integer[n-1-i])
+            output += letter * pow(10,i)
+
+        if negative==1:
+            output = -1*output
+        
+        if output>pow(2,31)-1 or output<-1*pow(2,31):
+            return 0
+
+        return output
