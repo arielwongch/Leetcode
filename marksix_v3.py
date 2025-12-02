5a2a7c894ec30d7b59822bcfe63a7de47d3cc4ad
Ariel <143091143+arielwongch@users.noreply.github.com>
2025-12-03 03:49:12 +0800

    updates

--- marksix_v3.py
+++ marksix_v3.py
@@ -2,7 +2,7 @@ import numpy as np
 from sklearn.preprocessing import normalize
 
 #change number of iterations
-num_iterations = 1
+num_iterations = 2
 
 #balls range from 1 to 49
 all_balls = np.arange(1, 50, dtype=int)
@@ -14,12 +14,14 @@ rng = np.random.default_rng()
 prob = rng.standard_normal(49)
 
 #normalize then make the sum of all prob to 1
-max_prob = max(prob)
-min_prob = min(prob)
-norm_prob = (prob - min_prob)/(max_prob - min_prob)
-norm_prob = norm_prob / sum(norm_prob)
+mean_prob = np.mean(prob)
+sd_prob = np.std(prob)
+norm_prob = (prob - mean_prob)/sd_prob
+norm_prob = norm_prob - min(norm_prob)
+norm_prob = norm_prob/ sum(norm_prob)
 
 #generate marksix result
+
 for _ in range(num_iterations):
     result = np.sort(rng.choice(all_balls, 6, replace=False, p=norm_prob, shuffle=True))
     print(result)
