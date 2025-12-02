d01822be652125c9e5caa195f3c27bdb1846c27e
Ariel <143091143+arielwongch@users.noreply.github.com>
2025-12-03 03:58:32 +0800

    Update marksix_v5.py

--- marksix_v5.py
+++ marksix_v5.py
@@ -2,7 +2,7 @@ import numpy as np
 from sklearn.preprocessing import normalize
 
 #change number of iterations
-num_iterations = 1
+num_iterations = 2
 
 #balls range from 1 to 49
 all_balls = np.arange(1, 50, dtype=int)
@@ -11,7 +11,7 @@ all_balls = np.arange(1, 50, dtype=int)
 rng = np.random.default_rng()
 
 #generate probability for each entry
-prob = rng.standard_normal(49)
+prob = rng.random((49,))
 
 #normalize then make the sum of all prob to 1
 max_prob = max(prob)
