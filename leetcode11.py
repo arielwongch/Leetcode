b1a27ff2d61bd485443185c2b28c1ac3802a6c27
Ariel <143091143+arielwongch@users.noreply.github.com>
2026-07-04 23:30:31 +0800

    Update leetcode11.py

--- leetcode11.py
+++ leetcode11.py
@@ -2,3 +2,28 @@
 
 """
 
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+
+        n = len(height)
+        
+        left = 0
+        right = n-1
+
+        max_area = 0
+
+        while left<right:
+            current_area = (right-left)*min(height[left],height[right])
+            if current_area > max_area:
+                max_area = current_area
+            
+            if height[left]<= height[right]:
+                left += 1
+            else:
+                right -= 1
+        return max_area
+
+
+
+
+        
